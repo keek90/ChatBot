f376f4fe9f0520e52fe5a54fde7ea4bf2893be2f
sandyra5 <145011377+sandyra5@users.noreply.github.com>
2024-03-03 03:11:16 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from "react";
+import {
+  Route,BrowserRouter as Router,
+  Routes
+}from 'react-router-dom';
+import './App.css'
+import Home from './pages/Home/Home';
+import Chat from './pages/Home/Home';
+import Profile from './pages/Profile/Profile';
+import  Signup from './pages/Signup/Signup';
+import Login from './pages/Login/Login';
+import {toast,ToastContainer} from 'react-toastify';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  showToast = (type,message) =>{
+    switch(type){
+      case 0:
+        toast.warning(message)
+        break;
+      case 1:
+        toast.warning(message)  
+        default:
+          break;
+
+    }
+  }
+  render(){
+    return (
+      <Router>
+        <ToastContainer
+        autoClose = {2000}
+        hideProgressBar={true}
+        position={toast.POSITION.BOTTOM_CENTER}
+        />
+        <Routes>
+          <Route
+          exact
+          path="/"
+          render={ props => <Home{...props}/>}/>
+
+         <Route
+         path = "/login"
+         render={props => <Login showToast={this.showToast}{...props}/>}
+         />
+         <Route
+         path = "/profile"
+         render={props => <Profile showToast={this.showToast}{...props}/>}
+         />
+         <Route
+         path = "/signup"
+         render={props => <Signup showToast={this.showToast}{...props}/>}
+         />
+         <Route
+         path = "/Chat"
+         render={props => <Chat showToast={this.showToast}{...props}/>}
+         />
+        </Routes>
+      </Router>
+    )
+  }
 }
-
 export default App;
+
